2b7e5b1f5d487f529cf219efa6b71847437f9384
Stanley <sjanesko12@gmail.com>
2019-05-08 20:04:21 -0500

    graph filled

--- Graph.py
+++ Graph.py
@@ -64,24 +64,100 @@ class Graph:
             vert.visited = False
             vert.distance = math.inf
 
+    def fillGraph(self):
+        '''Hard coded zipCode graph'''
+        # First row of zipCodes
+        self.addVert('64111')
+        self.addVert('64112')
+        self.addVert('64113')
+        self.addVert('64114')
+        self.addVert('64115')
+
+        # Second row of zipCodes
+        self.addVert('65111')
+        self.addVert('65112')
+        self.addVert('65113')
+        self.addVert('65114')
+        self.addVert('65115')
+
+        # Third row of zipCodes
+        self.addVert('65211')
+        self.addVert('65212')
+        self.addVert('65213')
+        self.addVert('65214')
+        self.addVert('65215')
+
+        # Fourth row of zipCodes
+        self.addVert('66111')
+        self.addVert('66112')
+        self.addVert('66113')
+        self.addVert('66114')
+        self.addVert('66115')
+
+        # Fifth row of zipCodes
+        self.addVert('67311')
+        self.addVert('67312')
+        self.addVert('67313')
+        self.addVert('67314')
+        self.addVert('67315')
+
+        # Add edges for first row of zipCodes
+        self.addEdge('64111', '64112', 3)
+        self.addEdge('64112', '64113', 7)
+        self.addEdge('64113', '64114', 2)
+        self.addEdge('64114', '64115', 1)
+
+        self.addEdge('64111', '65111', 9)
+        self.addEdge('64112', '65112', 1)
+        self.addEdge('64113', '65113', 3)
+        self.addEdge('64114', '65114', 7)
+        self.addEdge('64115', '65115', 2)
+
+        # Add edges for second row of zipCodes
+        self.addEdge('65111', '65112', 2)
+        self.addEdge('65112', '65113', 5)
+        self.addEdge('65113', '65114', 11)
+        self.addEdge('65114', '65115', 10)
+
+        self.addEdge('65111', '65211', 1)
+        self.addEdge('65112', '65212', 2)
+        self.addEdge('65113', '65213', 1)
+        self.addEdge('65114', '65214', 2)
+        self.addEdge('65115', '65215', 4)
+
+        # Add edges for third row of zipCodes
+        self.addEdge('65211', '65212', 8)
+        self.addEdge('65212', '65213', 3)
+        self.addEdge('65213', '65214', 5)
+        self.addEdge('65214', '65215', 1)
+
+        self.addEdge('65211', '66111', 3)
+        self.addEdge('65212', '66112', 2)
+        self.addEdge('65213', '66113', 6)
+        self.addEdge('65214', '66114', 7)
+        self.addEdge('65215', '66115', 4)
+
+        # Add edges for fourth row of zipCodes
+        self.addEdge('66111', '66112', 7)
+        self.addEdge('66112', '66113', 1)
+        self.addEdge('66113', '66114', 4)
+        self.addEdge('66114', '66115', 2)
+
+        self.addEdge('66111', '67311', 4)
+        self.addEdge('66112', '67312', 3)
+        self.addEdge('66113', '67313', 7)
+        self.addEdge('66114', '67314', 1)
+        self.addEdge('66115', '67315', 3)
+
+        # Add edges for fifth row of zipCodes
+        self.addEdge('67311', '67312', 1)
+        self.addEdge('67312', '67313', 5)
+        self.addEdge('67313', '67314', 10)
+        self.addEdge('67314', '67315', 11)
+
+        # print(self.vertDict['test'].getAdjNodes())
+
 
 if __name__ == '__main__':
     g = Graph()
-    g.addVert('a')
-    g.addVert('b')
-    g.addVert('c')
-    g.addVert('d')
-    g.addVert('e')
-    g.addVert('f')
-
-    g.addEdge('a', 'b', 7)
-    g.addEdge('a', 'c', 9)
-    g.addEdge('a', 'f', 14)
-    g.addEdge('b', 'c', 10)
-    g.addEdge('b', 'd', 15)
-    g.addEdge('c', 'd', 11)
-    g.addEdge('c', 'f', 2)
-    g.addEdge('d', 'e', 6)
-    g.addEdge('e', 'f', 9)
-
-    print(g.vertDict['a'].getAdjNodes())
+    g.fillGraph()
--- VehicleDispatchSystem.py
+++ VehicleDispatchSystem.py
@@ -1,10 +1,6 @@
-
+import Graph
 import pprint
-graph = {
-    'a': {'b': 1, 'c':  4},
-    'b': {'c':  3, 'd':  2, 'e':  2},
-    'c': {},
-    'd': {'b':  1, 'c':  5},
-    'e': {'d': -2}
-}
-pprint.pprint(graph)
+
+g = Graph.Graph()
+g.fillGraph()
+print(g.vertDict)
